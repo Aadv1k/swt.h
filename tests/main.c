b70ef804a1f44cb28c5db0c468a08676b8e7b580
aadv1k <aadv1k@outlook.com>
2023-08-16 15:30:32 +0530

    Add (Failing) tests for CCA

--- tests/main.c
+++ tests/main.c
@@ -1,12 +1,22 @@
 #include "../thirdparty/munit.h"
 
-extern MunitTest GrayscaleTests[] ;
+extern MunitTest GrayscaleTests[];
+extern MunitTest CCATests[];
 
 static MunitSuite test_suites[] = {
-    { (char*) "GrayscaleTests", GrayscaleTests, NULL, 1, MUNIT_SUITE_OPTION_NONE },
+    //{ (char*) "Grayscale_Tests", GrayscaleTests, NULL, 1, MUNIT_SUITE_OPTION_NONE },
+    { (char*) "CCA_Tests", CCATests, NULL, 1, MUNIT_SUITE_OPTION_NONE },
     { NULL, NULL, NULL, 0, MUNIT_SUITE_OPTION_NONE }
 };
 
+static MunitSuite main_suite = {
+    (char*) "",
+    NULL,
+    test_suites, 
+    1,
+    MUNIT_SUITE_OPTION_NONE 
+};
+
 int main(int argc, char* argv[]) {
-    return munit_suite_main(&test_suites[0], NULL, argc, argv);
+    return munit_suite_main(&main_suite, NULL, argc, argv);
 }
